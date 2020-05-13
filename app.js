3191edf5cc7bc13b1f205201060456b2b1924ffe
Prasoon <it5prasoon@users.noreply.github.com>
2020-05-13 13:43:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const forecast = require('./utils/forecast')
 
 
 geocode('Boston', (error, data) => {
-    console.log('Error!', error)
-    console.log('Data:', data)
+    if(error) {
+        return console.log(error)
+    }
 
-    forecast(data.latitude, data.longitude, (error, data) => {
-        console.log('Error!',error)
-        console.log('Data:', data)
+
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if(error) {
+            return console.log(error)
+        }
+        
+        
+        console.log(data.location)
+        console.log(forecastData)
     })
 })
