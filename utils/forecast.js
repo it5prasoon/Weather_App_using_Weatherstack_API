ed20842925eb5346787aa2df0e7fda449bc08ad5
Prasoon <it5prasoon@users.noreply.github.com>
2020-05-13 13:30:52 +0530

    Added Location and Forecast Api

---
+++ utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=0a6159ee767410e3c2cb37339cfd74b2&query='+latitude+ ',' + longitude + '&units=m'
+
+
+    request({url: url, json: true}, (error,response) =>{
+        if(error) {
+            callback('Unable to connect to weather services.', undefined)
+        } else if(response.body.error) {
+            callback('Unable to find location', undefined)
+        }
+        else{
+            callback(undefined, response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.')
+        }
+    })
+}
+
+module.exports = forecast
